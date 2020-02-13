0c01c445540686fc48d1ed363d74c3c7e45eba3d
alvaro <pulma_2@hotmail.com>
2020-02-13 16:19:29 +0100

    new file php

---
+++ NVL-PHP-FUNC-1/NVL-PHP-FUNC-1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        function cuentaLetraA($palabra = "amapola") {
+            for(i=0; i<7; i++){
+                if($palabras == "a"){
+                    echo "El número de letra a que tiene amapola es:" .count($palabra);
+                }
+            }  
+        }
+        cuentaLetraA();
+    </script>
+</body>
+</html>
